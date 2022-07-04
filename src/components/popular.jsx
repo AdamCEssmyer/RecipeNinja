dcee6ed705e6c3c8d07aa08c06da073cdcebd09b
Adam Essmyer <acemyer@gmail.com>
2022-07-04 14:46:35 -0500

    generated new API key

--- src/components/popular.jsx
+++ src/components/popular.jsx
@@ -8,7 +8,9 @@ function Popular() {
     },[]);
 
 const getPopular = async () => {
-    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number9`);
+    const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+        );
     const data = await api.json();
     console.log(data);
 }
