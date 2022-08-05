f15005fec7a6ae7a6e44a6836dda5b615f814a67
Adam Essmyer <acemyer@gmail.com>
2022-08-04 20:32:19 -0500

    added react splide for carousel for recipe cards

--- src/components/popular.jsx
+++ src/components/popular.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-
-
+import styled from "styled-components";
+import {Splide, SplideSlide} from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
 
 function Popular() { 
 
@@ -24,13 +25,37 @@ const getPopular = async () => {
 
  {popular.map((recipe) => {
     return(
-        <div key={recipe.id}>
-            <p>{recipe.title}</p>
-        </div>
+       <Wrapper>
+        <h3>Popular Picks</h3>
+        {popular.map((recipe) => {
+            return(
+                <Card>
+                    <p>{recipe.title}</p>
+                    <img src={recipe.image} alt="recipe.title"  />
+                </Card>
+            );
+        })}
+       </Wrapper>
     );
  })}
 
  </div>;
 }
+
+
+const Wrapper = styled.div
+`margin: 4rem 0rem`;
+
+const Card = styled.div
+`min-height: 25rem;
+border-radius: 2rem;
+overflow: hidden;
+
+img{
+    border-radius: 2rem;
+}
+
+
+`;
 
 export default Popular;
